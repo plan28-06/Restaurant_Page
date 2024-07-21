d69091961d82a38e0d3a89d085754d9fe0355e0a
plan28-06 <planar2006@gmail.com>
2024-07-21 08:03:04 +0530

    Added and styled the home page.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import "../src/style.css" 
+import "./style.css"
+const container=document.querySelector('#container');
+container.classList.add("homepage");
+
+function loadHomePage(){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    if (container.classList[0]!='homepage'){
+        container.classList.toggle('homepage')
+    }
+}
